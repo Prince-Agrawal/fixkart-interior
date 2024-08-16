3369f7ccaea1fe7fbffc11f139df1fbd4ab482fa
prince-agrawal <utkarshagrawal2022@gmail.com>
2024-08-16 15:34:48 +0530

    handle cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+// Use CORS middleware
+app.use(cors());
+
+// Middleware to parse JSON
+app.use(express.json());
 
 // Import Routes
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const contactRoutes = require('./routes/contactRoutes');
 
-// Middleware to parse JSON
-app.use(express.json());
+
 
 // Use routes
 app.use('/api/auth', authRoutes);  // Auth routes (e.g., /api/auth/register, /api/auth/login)
