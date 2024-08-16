2025676c98123c7518444c92c3c80cc2f5e7dbb9
prince-agrawal <utkarshagrawal2022@gmail.com>
2024-08-16 15:36:01 +0530

    admin login client side handle

--- client/src/routes/AppRoutes.js
+++ client/src/routes/AppRoutes.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
+import AdminLogin from '../pages/admin/AdminLogin';
 
 const AppRoutes = () => {
     return (
@@ -12,6 +13,9 @@ const AppRoutes = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
+
+                {/* Admin Routes */}
+                <Route path="/admin/login" element={<AdminLogin />} />
             </Routes>
         </Router>
     );
