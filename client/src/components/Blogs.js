8d46c328b0ad99693b6cb6617810577985c22d48
prince-agrawal <utkarshagrawal2022@gmail.com>
2024-09-08 19:23:48 +0530

    blog listing

--- client/src/components/Blogs.js
+++ client/src/components/Blogs.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
+import axios from "axios";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Blogs = () => {
-    var settings = {
-        autoplay: true,
-        autoplaySpeed: 3000, // Slow down the autoplay speed to 3 seconds
-        centerMode: false,
-        slidesToShow: 2, // Show 1 slide at a time
-        slidesToScroll: 2,
-        variableWidth: false,
-        infinite: true,
-        initialSlide: 0, // Start from the first slide
-        arrows: false,
-        dots: true,
-      };
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/blogs`);
+        setBlogs(response.data);
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      }
+    };
+
+    fetchBlogs();
+  }, []);
+
+  var settings = {
+    autoplay: true,
+    autoplaySpeed: 3000, // Slow down the autoplay speed to 3 seconds
+    centerMode: false,
+    slidesToShow: 2, // Show 1 slide at a time
+    slidesToScroll: 2,
+    variableWidth: false,
+    infinite: true,
+    initialSlide: 0, // Start from the first slide
+    arrows: false,
+    dots: true,
+  };
+
+  // Function to format date as "Month Day, Year"
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
   return (
     <>
       <section className="BlogSection slider-dots">
@@ -32,8 +54,8 @@ const Blogs = () => {
           </div>
 
           <div className="BlogSlider">
-             <Slider {...settings}>
-             <div>
+            <Slider {...settings}>
+              {/* <div>
                     <div className="BlogBox">
                         <img src="images/living-room.png" alt="Blog" className="blog-image" />
                         <h3>Trendy and Chic: Unveiling the New Elite Laminate Collection</h3>
@@ -76,8 +98,30 @@ const Blogs = () => {
                         <button type="button" className="btn btn-outline-primary ms-auto">Read More</button>
                         </div>
                     </div>
-             </div>
-             </Slider>
+             </div> */}
+
+              {blogs.map((blog) => (
+                <div key={blog._id}>
+                  <div className="BlogBox">
+                    <img
+                      src={process.env.REACT_APP_API_BASE_URL + '/' + blog.imagePath || "/images/living-room.png"}
+                      alt={blog.title}
+                      className="blog-image"
+                    />
+                    <h3>{blog.title}</h3>
+                    <p>{blog.description}</p>
+                    <div className="blog-user">
+                      <img src="images/user.png" alt="Blog user" className="user" />
+                      <span>{blog.addedBy}</span> | <span>{formatDate(blog.createdAt)}</span>
+                      <button type="button" className="btn btn-outline-primary ms-auto">
+                        Read More
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+
+            </Slider>
           </div>
         </div>
       </section>
