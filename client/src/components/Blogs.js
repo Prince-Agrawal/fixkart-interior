7d69686ce2b69762313dec4507ed0fa78269d118
prince-agrawal <utkarshagrawal2022@gmail.com>
2024-11-09 19:18:30 +0530

    manage home page redirection

--- client/src/components/Blogs.js
+++ client/src/components/Blogs.js
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import axios from "axios";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Link, useNavigate } from "react-router-dom";
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
@@ -42,6 +43,17 @@ const Blogs = () => {
     ],
   };
 
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const navigate = useNavigate();
+
+    const handleMoreNewsClick = () => {
+        scrollToTop();
+        navigate('/blog'); // Redirect to /blogs
+    };
+
   // Function to format date as "Month Day, Year"
   const formatDate = (dateString) => {
     const options = { year: "numeric", month: "long", day: "numeric" };
@@ -58,73 +70,38 @@ const Blogs = () => {
             <h2 className="d-flex align-items-end">
               Take a look at our latest <br />
               articles & resources
-              <button className="btn btn-primary ms-auto">More News</button>
+              <button onClick={handleMoreNewsClick} className="btn btn-primary ms-auto">More News</button>
             </h2>
           </div>
 
           <div className="BlogSlider">
             <Slider {...settings}>
-              {/* <div>
-                    <div className="BlogBox">
-                        <img src="images/living-room.png" alt="Blog" className="blog-image" />
-                        <h3>Trendy and Chic: Unveiling the New Elite Laminate Collection</h3>
-                        <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
-                        <div className="blog-user">
-                        <img src="images/user.png" alt="Blog user" className="user"/>
-                        <span>fixinterio</span> | 
-                        <span>September 1, 2022</span>
-
-                        <button type="button" className="btn btn-outline-primary ms-auto">Read More</button>
-                        </div>
-                    </div>
-             </div>
-
-             <div>
-                    <div className="BlogBox">
-                        <img src="images/living-room.png" alt="Blog" className="blog-image" />
-                        <h3>Trendy and Chic: Unveiling the New Elite Laminate Collection</h3>
-                        <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
-                        <div className="blog-user">
-                        <img src="images/user.png" alt="Blog user" className="user"/>
-                        <span>fixinterio</span> | 
-                        <span>September 1, 2022</span>
-
-                        <button type="button" className="btn btn-outline-primary ms-auto">Read More</button>
-                        </div>
-                    </div>
-             </div>
-
-             <div>
-                    <div className="BlogBox">
-                        <img src="images/living-room.png" alt="Blog" className="blog-image"/>
-                        <h3>Trendy and Chic: Unveiling the New Elite Laminate Collection</h3>
-                        <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
-                        <div className="blog-user">
-                        <img src="images/user.png" alt="Blog user" className="user"/>
-                        <span>fixinterio</span> | 
-                        <span>September 1, 2022</span>
-
-                        <button type="button" className="btn btn-outline-primary ms-auto">Read More</button>
-                        </div>
-                    </div>
-             </div> */}
 
               {blogs.map((blog) => (
                 <div key={blog._id}>
                   <div className="BlogBox">
-                    <img
+                    {/* <img
                       src={process.env.REACT_APP_API_BASE_URL + '/' + blog.imagePath || "/images/living-room.png"}
                       alt={blog.title}
+                      className="blog-image"
+                    /> */}
+                    <img
+                      src={process.env.REACT_APP_API_BASE_URL + '/' + blog.imagePath || "/images/living-room.png"}
+                      alt="Blog"
                       className="blog-image"
                     />
                     <h3>{blog.title}</h3>
                     <p>{blog.description}</p>
                     <div className="blog-user">
-                      <img src="images/user.png" alt="Blog user" className="user" />
+                      <img
+                        src="/images/user.png"
+                        alt="Blog user"
+                        className="user"
+                      />
                       <span>{blog.addedBy}</span> | <span>{formatDate(blog.createdAt)}</span>
-                      <button type="button" className="btn btn-outline-primary ms-auto">
+                      <Link to={`/blog/${blog._id}`} className="btn btn-outline-primary ms-auto">
                         Read More
-                      </button>
+                      </Link>
                     </div>
                   </div>
                 </div>
